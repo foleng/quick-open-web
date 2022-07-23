d9e6dc950f6416353a1c5c2ebe08bc79a5e415fb
Felong <huangyi@tungee.com>
2022-07-24 00:21:29 +0800

    :fix: delete chrome1

--- chrome1/js/background.js
+++ chrome1/js/background.js
@@ -8,3 +8,24 @@ socket.addEventListener("message", event=>{
 console.log(">>>>>>>>>>>>>", event.data);
 window.open(event.data)
 })
+
+
+chrome.tabs.onActivated.addListener(moveToFirstPosition);
+
+async function moveToFirstPosition(activeInfo) {
+  try {
+    await chrome.tabs.move(activeInfo.tabId, {index: 0});
+    const tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    console.log("tabs:", tabs);  
+  } catch (error) {
+    if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
+      setTimeout(() => moveToFirstPosition(activeInfo), 50);
+    } else {
+      console.error(error);
+    }
+  }
+}
+
+function good(params) {
+  
+}
