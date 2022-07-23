cd9fee360c763e7e0d0c4db0c952819a858f5cc2
Felong <huangyi@tungee.com>
2022-07-24 00:19:55 +0800

    feat: 手机扫码popup打开当前pc链接

--- chrome/vite.config.ts
+++ chrome/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: './manifest.json', dest: 'dist' }
+      ],
+      hook: "writeBundle"
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
